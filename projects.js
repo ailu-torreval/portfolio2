3d6014e3ac4b390259d033806665fa591e1e61e1
ailin torre val <aili0057@stud.kea.dk>
2022-04-13 19:50:45 +0200

    scroll triggered animations added

--- projects.js
+++ projects.js
@@ -7,7 +7,15 @@ gsap.fromTo(
   { y: 20, duration: 1.5, ease: "power1.inOut", yoyo: true, repeat: -1 }
 );
 
-const sections = ["#ab-me", "#projects", "#contact-box"];
+const sections = [
+  ".proj-info",
+  ".mockups1",
+  ".phone1",
+  ".phone2",
+  ".phone3",
+  ".phone4",
+  ".phone5",
+];
 
 sections.forEach((section) => {
   gsap.to(section, {
