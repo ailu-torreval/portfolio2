b2c01dbfd9fdb3efe7334d1701ee6fbd0453e4f0
ailin torre val <aili0057@stud.kea.dk>
2022-04-14 17:42:11 +0200

    i think is finished

--- index-js.js
+++ index-js.js
@@ -1,6 +1,27 @@
 "use strict";
 gsap.registerPlugin(ScrollTrigger);
 
+document.querySelector(".burger-btn").addEventListener("click", openBurgerMenu);
+document
+  .querySelector(".burger-btn-nav")
+  .addEventListener("click", openBurgerMenu);
+
+function openBurgerMenu() {
+  document.querySelector(".burger-menu").classList.remove("hidden");
+  document.querySelector(".burger-menu").classList.add("burger-appear");
+  document
+    .querySelector(".close-btn")
+    .addEventListener("click", closeBurgerMenu);
+  document
+    .querySelector(".b-contact")
+    .addEventListener("click", closeBurgerMenu);
+  document.querySelector(".b-proj").addEventListener("click", closeBurgerMenu);
+}
+
+function closeBurgerMenu() {
+  document.querySelector(".burger-menu").classList.add("hidden");
+}
+
 gsap.fromTo(
   ".down-arrow",
   { y: -20 },
--- projects.js
+++ projects.js
@@ -1,6 +1,19 @@
 "use strict";
 gsap.registerPlugin(ScrollTrigger);
 
+document.querySelector(".burger-btn").addEventListener("click", openBurgerMenu);
+document
+  .querySelector(".burger-btn-nav")
+  .addEventListener("click", openBurgerMenu);
+
+function openBurgerMenu() {
+  document.querySelector(".burger-menu").classList.remove("hidden");
+  document.querySelector(".burger-menu").classList.add("burger-appear");
+  document.querySelector(".close-btn").addEventListener("click", function () {
+    document.querySelector(".burger-menu").classList.add("hidden");
+  });
+}
+
 gsap.fromTo(
   ".down-arrow",
   { y: -20 },
